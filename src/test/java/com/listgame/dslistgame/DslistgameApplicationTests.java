6ffecf238f12beea2cffbf5743df00b31dcdcd1d
Jonathas <jtsbarrosferreira@gmail.com>
2025-01-22 22:17:56 -0300

    Project created

---
+++ src/test/java/com/listgame/dslistgame/DslistgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listgame.dslistgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
