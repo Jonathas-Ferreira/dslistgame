32f037a1e95b0782365734423015768c7838b0a9
Jonathas <jtsbarrosferreira@gmail.com>
2025-02-03 22:43:53 -0300

    get games by list

--- src/main/java/com/listgame/dslistgame/services/GameService.java
+++ src/main/java/com/listgame/dslistgame/services/GameService.java
@@ -1,4 +1,21 @@
 package com.listgame.dslistgame.services;
 
+import com.listgame.dslistgame.dto.GameMinDTO;
+import com.listgame.dslistgame.entities.Game;
+import com.listgame.dslistgame.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<Game> findAll(){
+        List<Game> result = gameRepository.findAll();
+        return result;
+    }
 }
