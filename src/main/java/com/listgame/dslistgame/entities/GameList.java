a3ecb0ceb825a1ea070396dc44901d7039c52a21
Jonathas <jtsbarrosferreira@gmail.com>
2025-03-06 10:29:58 -0300

    compare list by id

--- src/main/java/com/listgame/dslistgame/entities/GameList.java
+++ src/main/java/com/listgame/dslistgame/entities/GameList.java
@@ -1,4 +1,47 @@
 package com.listgame.dslistgame.entities;
 
+import java.util.Objects;
+
 public class GameList {
+
+    private Long id;
+    private String name;
+
+    public GameList(){
+    }
+
+    public GameList(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long id(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String name(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    // Comparar duas list usando equals hascode, usando somente id
+    @Override
+    public int hashcode(){
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GameList other = (GameList) obj;
+        return Objects.equals(id, other.id);
+    }
 }
