efd8b76c42da177075eb0601075aa056dbbd29bb
Jonathas <jtsbarrosferreira@gmail.com>
2025-10-01 11:18:14 -0300

    add GameListDTO

---
+++ src/main/java/com/listgame/dslistgame/dto/GameListDTO.java
@@ -0,0 +1,26 @@
+package com.listgame.dslistgame.dto;
+
+import com.listgame.dslistgame.entities.GameList;
+
+public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO(){
+    }
+
+    public GameListDTO(GameList entity){
+        super();
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
